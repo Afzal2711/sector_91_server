8a2b59e858c2486495ebf577fa2d8f2bed7a0b61
Afzal2711 <113162361+Afzal2711@users.noreply.github.com>
2024-12-03 02:11:31 +0530

    created userSchema

---
+++ routes/UserLginRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import Login from '../models/UserLogin';
+
+const router = express.Router();
+
+router.post('/login', async (req, res) => {
+
+    const { Username, Email, Password } = req.body;
+
+    try{
+        const newUser = new Login({ Username, Email, Password });
+        await Login.save();
+        res.status(201).json({ message: "User logged in successfully",Login});
+    }
+
+    catch(error) {
+        console.error('error logging in:', error)
+        res.status(400).send('error logging in: '+error.message);
+    }
+});
